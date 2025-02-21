50eb3323c79512ac6784d55c61cd3f5155da8fe8
Musab Tulaimat <mamjt@hotmail.com>
2025-02-22 01:59:48 +0400

    added language totggle and word per difficulty dropdown

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("cardData", () => ({
-    currentLanguage: "english", // Default language
+    settings: {
+      cardsPerDifficulty: 3, // Default: Show 3 words per difficulty
+      language: "english",
+    },
 
     toggleLanguage() {
       console.log("Toggling language...");
-      this.currentLanguage =
-        this.currentLanguage === "english" ? "arabic" : "english";
+      this.settings.language =
+        this.settings.language === "english" ? "arabic" : "english";
+      console.log("Current Language:", this.settings.language);
+    },
+
+    updateCardsPerDifficulty(value) {
+      this.settings.cardsPerDifficulty = parseInt(value);
+      console.log("Words Per Difficulty:", this.settings.cardsPerDifficulty);
     },
 
     get data() {
       return {
-        easy: {
-          emoji: "😐",
-          words: {
-            english: ["Word 1", "Word 2", "Word 3"],
-            arabic: ["كلمة ١", "كلمة ٢", "كلمة ٣"],
+        cards: {
+          easy: {
+            order: 1,
+            emoji: "😐",
+            words: {
+              english: ["Word 1", "Word 2", "Word 3", "Word 4", "Word 5"],
+              arabic: ["كلمة ١", "كلمة ٢", "كلمة ٣", "كلمة ٤", "كلمة ٥"],
+            },
+            color: "#008000", // Green
           },
-          color: "#008000", // Green
-        },
-        medium: {
-          emoji: "🤔",
-          words: {
-            english: ["Word 4", "Word 5", "Word 6"],
-            arabic: ["كلمة ٤", "كلمة ٥", "كلمة ٦"],
+          medium: {
+            order: 4,
+            emoji: "🤔",
+            words: {
+              english: ["Word 6", "Word 7", "Word 8", "Word 9", "Word 10"],
+              arabic: ["كلمة ٦", "كلمة ٧", "كلمة ٨", "كلمة ٩", "كلمة ١٠"],
+            },
+            color: "#F47F00", // Orange
           },
-          color: "#F47F00", // Orange
-        },
-        hard: {
-          emoji: "🤯",
-          words: {
-            english: ["Word 7", "Word 8", "Word 9"],
-            arabic: ["كلمة ٧", "كلمة ٨", "كلمة ٩"],
+          hard: {
+            order: 7,
+            emoji: "🤯",
+            words: {
+              english: ["Word 11", "Word 12", "Word 13", "Word 14", "Word 15"],
+              arabic: ["كلمة ١١", "كلمة ١٢", "كلمة ١٣", "كلمة ١٤", "كلمة ١٥"],
+            },
+            color: "#FF0000", // Red
           },
-          color: "#FF0000", // Red
         },
       };
     },
